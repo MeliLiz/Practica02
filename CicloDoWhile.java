6a7971f8a5ce416d985ca103e565c25ce3ec5d85
MeliLiz <melifernandez@ciencias.unam.mx>
2021-11-04 20:44:16 -0600

    Documentado

--- CicloDoWhile.java
+++ CicloDoWhile.java
@@ -1,25 +1,48 @@
-public class CicloDoWhile{
-    public static void main (String[] args){
+/**
+ * Clase CicloDoWhile Clase que imprime todos los números primos del 0 al 1000
+ * 
+ * @author Melissa Fernández
+ * @version 1.0
+ */
+
+public class CicloDoWhile {
+    /**
+     * Método principal donde se ejecutan los métodos do while para ver si los
+     * números son primos o no
+     * 
+     * @param args Los argumentos
+     */
+    public static void main(String[] args) {
+        // Contador para saber cuántos números primos hay del 0 al 1000
         int contador = 0;
-        int numero =2;
+        // El número que queremos verificar si es primo o no
+        int numero = 2;
+        // Ciclo do while que hace que el número vaya hasta 999
         do {
-            boolean esPrimo=true;
+            // esPrimo: booleano que inicia en true
+            boolean esPrimo = true;
+            // El divisor empieza en 2
             int divisor = 2;
-            do{
-                if (numero % divisor == 0){
-                    esPrimo = false;
+            // Ciclo do while para tomar a los posibles divisores
+            do {
+                // Condicional para saber si el número tiene divisores
+                if (numero % divisor == 0) {
+                    esPrimo = false;// Si el número tiene algún divisor, esPrimo cambia a false
                 }
-                divisor++;     
-            } while (divisor < numero/2);
-            if (numero ==2){
+                divisor++; // el divisor aumenta 1
+            } while (divisor < numero / 2);
+            // Condicional en el que si el número es 2, esPrimo se hace true
+            if (numero == 2) {
                 esPrimo = true;
             }
-            if (esPrimo){
-                System.out.print(numero + " ");
-                contador++;
+            // Condicional que verifica si esPrimo se mantiene en true
+            if (esPrimo) {
+                System.out.print(numero + " "); // Si esPrimo =true imprimir el número
+                contador++;// El contador aumenta 1
             }
-            numero++;
+            numero++; // El número aumenta 1
         } while (numero < 1000);
-        System.out.println("\nHay "+ contador + " números primos entre 0 y 1000.");
+        // Imprimir cuántos números primos hay de 0 a 1000
+        System.out.println("\nHay " + contador + " números primos entre 0 y 1000.");
     }
 }
--- CicloWhile.java
+++ CicloWhile.java
@@ -1,22 +1,43 @@
-public class CicloWhile{
-    public static void main (String[] args){
-        int contador=0;
-        int numero =2;
-        while (numero <1000){
+/**
+ * Clase CicloWhile Clase que imprime todos los números primos del 0 al 1000
+ * 
+ * @author Melissa Fernández
+ * @version 1.0
+ */
+public class CicloWhile {
+    /**
+     * Método principal donde se ejecutan los métodos while para ver si los números
+     * son primos o no
+     * 
+     * @param args Los argumentos
+     */
+    public static void main(String[] args) {
+        // Contador para saber cuántos números primos hay del 0 al 1000
+        int contador = 0;
+        // Ciclo while que hace que el número vaya hasta 999
+        // El número que queremos verificar si es primo o no
+        int numero = 2;
+        while (numero < 1000) {
+            // esPrimo: booleano que inicia en true
             boolean esPrimo = true;
-            int divisor = 2;       
-            while (divisor <= numero/2){
-                if (numero % divisor == 0){
-                    esPrimo = false;
+            // Ciclo while para tomar a los posibles divisores
+            // El divisor empieza en 2
+            int divisor = 2;
+            while (divisor <= numero / 2) {
+                // Condicional para saber si el número tiene divisores
+                if (numero % divisor == 0) {
+                    esPrimo = false; // Si el número tiene algún divisor, esPrimo cambia a false
                 }
-                divisor++;
+                divisor++; // el divisor aumenta 1
             }
-            if (esPrimo){
-                System.out.print(numero + " ");
-                contador++;
+            // Condicional que verifica si esPrimo se mantiene en true
+            if (esPrimo) {
+                System.out.print(numero + " "); // Si esPrimo=true, se imprime el número
+                contador++;// El contador aumenta 1
             }
-            numero++;
+            numero++; // El número aumenta 1
         }
+        // Imprimir cuántos números primos hay de 0 a 1000 usando el contador.
         System.out.println("\nHay " + contador + " números primos entre 0 y 1000");
-    }    
+    }
 }
